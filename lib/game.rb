9b4eb8ac75d988f60a5d2bec9498f4cb2d713494
katebeavis <katebeavis84@gmail.com>
2015-04-30 16:59:22 +0100

    refactored game to case statement

--- lib/game.rb
+++ lib/game.rb
@@ -14,14 +14,11 @@ class Game
   end
 
   def winner?(player1, player2)
-    if player1.choice == player2.choice
-      'It\'s a draw!'
-    elsif player1.choice == 'scissors' && player2.choice == 'paper'
-      "#{player1.name} wins!"
-    elsif player1.choice == 'paper' && player2.choice == 'rock'
-      "#{player1.name} wins!"
-    elsif player1.choice == 'rock' && player2.choice == 'scissors'
-      "#{player1.name} wins!"
+    case
+    when player1.choice == player2.choice then'It\'s a draw!'
+    when player1.choice == 'scissors' && player2.choice == 'paper' then "#{player1.name} wins!"
+    when player1.choice == 'paper' && player2.choice == 'rock' then "#{player1.name} wins!"
+    when player1.choice == 'rock' && player2.choice == 'scissors' then "#{player1.name} wins!"
     else
       "#{player2.name} wins!"
     end
